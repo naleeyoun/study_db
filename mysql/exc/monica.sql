6fab13a104a45ef5549812914b991d0755a015c3
ina <naleeyoun@naver.com>
2022-02-09 14:16:12 +0900

    monica 수업

--- mysql/exc/monica.sql
+++ mysql/exc/monica.sql
@@ -7,17 +7,24 @@ select
     , b.ifcdName
     , b.ifcdOrder
     , b.ifcdUseNy
+    , b.ifcdDelNy
 from infrcodegroup a
 	left join infrCode b on b.ifcgSeq = a.ifcgSeq
 where 1=1
 	and a.ifcgDelNy = 0
 	and a.ifcgUseNy = 1
 	and b.ifcdDelNy = 0
-    and b.ifcdUseNy = 1 
+	and b.ifcdUseNy = 1 
     -- and a.ifcgSeq = 3
 order by 
-	a.ifcgSeq asc
-    , b.ifcdOrder desc;
+    a.ifcgseq asc
+	, a.ifcgName asc
+    , b.ifcdSeq desc
+    , b.ifcdName desc
+    , b.ifcdOrder desc
+    , b.ifcdUseNy desc
+    , b.ifcdDelNy desc
+;
 
 select * from infrCode;
 
